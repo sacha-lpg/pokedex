cf3872aa1bcb508d02863d6a000b79fe52838da8
LEPAGE <sacha.lepage@atos.net>
2021-11-03 14:28:02 +0100

    forms fixed

--- src/component/forms/selectInput.js
+++ src/component/forms/selectInput.js
@@ -2,20 +2,20 @@ import React from "react";
 import { useField, ErrorMessage } from "formik";
 import {StyledLabel, StyledSelect, StyledErrorMessage } from './styled';
 
-const Select = ({label, ...props}: any) = {
+const Select = ({ label, ...props }: any) => {
     const [field, meta] = useField(props);
-}
 
-return (
-    <>
-        <StyledLabel htmlFor={props.id || props.name}>{label}</StyledLabel>
-        <StyledSelect
-            {...field}
-            {...props}
-            style={meta.touched && meta.error ? { borderColor: 'red'} : null}
-        />
-        <ErrorMessage component={StyledErrorMessage} name={props.id || props.name} />
-    </>
-);
-
+    return (
+        <>
+            <StyledLabel htmlFor={props.id || props.name}>{label}</StyledLabel>
+            <StyledSelect
+                {...field}
+                {...props}
+                style={meta.touched && meta.error ? { borderColor: 'red'} : null}
+            />
+            <ErrorMessage component={StyledErrorMessage} name={props.id || props.name} />
+        </>
+    );
 };
+
+export default Select;
